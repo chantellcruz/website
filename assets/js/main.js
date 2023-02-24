e233379b636f63260c2d916aa35b39d88a44536e
Chantell <chantellraecruz@gmail.com>
2023-02-23 20:37:05 -0600

    added contact section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
 /*========== ˗ˏˋ ✿ T H E M E  T O G G L E ✿ ´ˎ˗ ==========*/
 const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
 
-themeButton.onclick = () => {
-    themeButton.classList.toggle('bxs-moon');
-    document.body.classList.toggle('dark-theme')
-    themeButton.classList.toggle('bxs-sun');
+// previously selected theme
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// otain current theme by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bxs-moon' : 'bx bxs-sun'
+
+// validate if the user previously chose a theme
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bxs-sun' ? 'add' : 'remove'](iconTheme)
 }
+// activate / deactivate the theme with the button
+themeButton.addEventListener('click', () => {
+    // add or remove the dark icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // save the theme and that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
